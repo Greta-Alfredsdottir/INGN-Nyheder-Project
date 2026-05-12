1dc0e94f9426d6826e7b5046088b3ed920e3951d
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-05-12 14:59:02 +0200

    dato

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -5,14 +5,15 @@ export const ArticleCards = ({NyhedsCard}) => {
     if (!NyhedsCard) {
         return null;
     }
- const date = (new Date(NyhedsCard.dato).getDate()+"/"+ new Date(NyhedsCard.dato).getMonth()+"-"+new Date(NyhedsCard.dato).getFullYear());
+    const dateObj = new Date(NyhedsCard.dato);
+    const date = `${dateObj.getDate()}/${dateObj.getMonth() + 1}-${dateObj.getFullYear()}`;
 
     return(
         <section className={style.cardContainer}>
             <div className={style.cardContent}>
             <h1 className={style.overskrift}>{NyhedsCard.overskrift}</h1>
             <p className={style.indhold}>{NyhedsCard.indhold?.text}</p>
-            <p className={style.dato}>D. {NyhedsCard.dato} - af {NyhedsCard.skribent}</p>
+            <p className={style.dato}>D. {date} - af {NyhedsCard.skribent}</p>
             <p><button className="minButton">Se mere</button></p>
             {NyhedsCard.billeder?.url && <img className={style.billeder} src={NyhedsCard.billeder.url} alt=""/>}
             </div>
